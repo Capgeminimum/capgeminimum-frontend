249f24ad6d8c52f414f3a90fd2f0050a23658ae5
Loic <loic.andrianarivony@ynov.com>
2026-03-31 14:16:37 +0200

    Fix route configuration in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,6 @@
 import Tournament from "./pages/Tournament"
 import TournamentDetails from "./pages/TournamentDetails"
 import CreateTournament from "./pages/CreateTournament"
-
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/tournois" element={<Tournament />} />
-        <Route path="/tournois/:id" element={<TournamentDetails />} />
-        <Route path="/create-tournament" element={<CreateTournament />} />
-      </Routes>
-    </BrowserRouter>
-  )
-}
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Leaderboard from "./pages/Leaderboard";
@@ -22,6 +10,9 @@ function App() {
   return (
     <div>
       <Routes>
+        <Route path="/tournois" element={<Tournament />} />
+        <Route path="/tournois/:id" element={<TournamentDetails />} />
+        <Route path="/create-tournament" element={<CreateTournament />} />
         <Route path="/" element={<HomePage />} />
         <Route path="/leaderboard" element={<Leaderboard />} />
         <Route path="/profil" element={<Profil />} />
