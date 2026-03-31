f58c2af27d6b5834f49e2d24a50c713be2a9dff0
lanathan <nathan.lamarche@ynov.com>
2026-03-31 10:40:18 +0200

    feat(HomePage) : added the toornament card

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,13 @@
 import Header from "../components/Header";
-import Podium from "../components/Podium"
+import Podium from "../components/Podium";
+import ToornamentCard from "../components/ToornamentCard";
 
 function HomePage() {
   return (
     <div>
       <Header />
       <main style={{ padding: "20px", marginTop:"80px" }}>
+        <ToornamentCard /> 
       </main>
       <Podium />
     </div>
